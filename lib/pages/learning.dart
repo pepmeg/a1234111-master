71e5a8748ababb7c1ff81d3272b736c1ac4686d0
XeniaLucky <erofeefa5@gmail.com>
2025-07-08 11:00:20 +0300

    two

--- lib/pages/learning.dart
+++ lib/pages/learning.dart
@@ -13,10 +13,11 @@ class _LearningState extends State<Learning> {
         preferredSize: const Size.fromHeight(70),
         child: AppBar(
           backgroundColor: const Color(0xFFF5F7FB),
+          elevation: 0,
           title: Padding(
             padding: const EdgeInsets.only(left: 30, top: 30),
             child: RichText(
-              text: TextSpan(
+              text: const TextSpan(
                 children: [
                   TextSpan(
                     text: 'Mock ',
@@ -40,29 +41,95 @@ class _LearningState extends State<Learning> {
           ),
         ),
       ),
-      body: Stack(
-        child: Padding(padding: EdgeInsets.symmetric(horizontal: 30),
-          child:
-          Row(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Stack(
+          children: [
+            Row(
               crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Padding(
-                    padding: EdgeInsets.only(right: 11),
-                    child: Container(
-                      width: 49,
-                      height: 38,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(16)),
-                      alignment: Alignment.center,
-                      child: Text(
-                        'All',
-                        style: TextStyle(fontSize: 14, color: Colors.black),
+                Container(
+                  width: 49,
+                  height: 38,
+                  margin: const EdgeInsets.only(right: 11),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF841ffd),
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        blurRadius: 10,
+                        offset: const Offset(0, 4),
                       ),
-                    ))
-              ])
-    )
+                    ],
+                  ),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'All',
+                    style: TextStyle(fontSize: 14, color: Colors.white),
+                  ),
+                ),
+                Container(
+                  width: 109,
+                  height: 38,
+                  margin: const EdgeInsets.only(right: 11),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        blurRadius: 10,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'In Progress',
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
+                ),
+                Container(
+                  width: 112,
+                  height: 38,
+                  margin: const EdgeInsets.only(right: 11),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        blurRadius: 10,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'Completed',
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 60),
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    blurRadius: 10,
+                    offset: const Offset(0, 4),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
